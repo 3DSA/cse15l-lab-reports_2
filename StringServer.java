9a058791a31aeb9e81661899faac41bab2b49b8f
3DSA <146782672+3DSA@users.noreply.github.com>
2023-10-22 19:41:43 -0700

    Update StringServer.java

--- StringServer.java
+++ StringServer.java
@@ -5,20 +5,25 @@ class Handler implements URLHandler {
     // The one bit of state on the server: a number that will be manipulated by
     // various requests.
     int num = 0;
+    private static ArrayList<String> messages = new ArrayList<>();
 
     public String handleRequest(URI url) {
         if (url.getPath().equals("/")) {
-            return String.format("Number: %d", num);
-        } else if (url.getPath().equals("/increment")) {
-            num += 1;
-            return String.format("Number incremented!");
-        } else {
-            if (url.getPath().contains("/add")) {
-                String[] parameters = url.getQuery().split("=");
-                if (parameters[0].equals("count")) {
-                    num += Integer.parseInt(parameters[1]);
-                    return String.format("Number increased by %s! It's now %d", parameters[1], num);
-                }
+            StringBuilder response = new StringBuilder();
+           for (int i = 0; i < messages.size(); i++) {
+                response.append((i + 1) + ". " + messages.get(i) + "\n");
+            }
+        } 
+        else {
+            if (url.getPath().contains("/add-message?s")) {
+                String parameters = url.getQuery().split("=");
+                messages.add(parameters);
+                StringBuilder response = new StringBuilder();
+                for (int i = 0; i < messages.size(); i++) {
+                response.append((i + 1) + ". " + messages.get(i) + "\n");
+            }
+            return response.toString();   
+               
             }
             return "404 Not Found!";
         }
